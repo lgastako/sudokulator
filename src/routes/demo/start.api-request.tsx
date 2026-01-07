f4f99781e16f5a4d1f336196fae770e9f626de0e
John Evans <lgastako@gmail.com>
2026-01-07 00:04:18 -0500

    chkpt

--- src/routes/demo/start.api-request.tsx
+++ src/routes/demo/start.api-request.tsx
@@ -14,27 +14,27 @@ function Home() {
   const [names, setNames] = useState<Array<string>>([])
 
   useEffect(() => {
-    getNames().then(setNames)
-  }, [])
+    getNames().then(setNames).catch(console.error)
+  }, [getNames])
 
   return (
     <div
-      className="flex items-center justify-center min-h-screen p-4 text-white"
+      className="flex justify-center items-center p-4 min-h-screen text-white"
       style={{
         backgroundColor: '#000',
         backgroundImage:
           'radial-gradient(ellipse 60% 60% at 0% 100%, #444 0%, #222 60%, #000 100%)',
       }}
     >
-      <div className="w-full max-w-2xl p-8 rounded-xl backdrop-blur-md bg-black/50 shadow-xl border-8 border-black/10">
-        <h1 className="text-2xl mb-4">Start API Request Demo - Names List</h1>
-        <ul className="mb-4 space-y-2">
+      <div className="bg-black/50 shadow-xl backdrop-blur-md p-8 border-8 border-black/10 rounded-xl w-full max-w-2xl">
+        <h1 className="mb-4 text-2xl">Start API Request Demo - Names List</h1>
+        <ul className="space-y-2 mb-4">
           {names.map(name => (
             <li
               key={name}
-              className="bg-white/10 border border-white/20 rounded-lg p-3 backdrop-blur-sm shadow-md"
+              className="bg-white/10 shadow-md backdrop-blur-sm p-3 border border-white/20 rounded-lg"
             >
-              <span className="text-lg text-white">{name}</span>
+              <span className="text-white text-lg">{name}</span>
             </li>
           ))}
         </ul>
